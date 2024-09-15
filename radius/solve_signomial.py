0dc66b650a4e900af0104772fc180806bdf81ed5
Zakariae MOUTAOUAKIL <zakariae.moutaouakil@etu.u-paris.fr>
2024-09-15 14:52:28 +0200

    Added hyperopt

--- radius/solve_signomial.py
+++ radius/solve_signomial.py
@@ -1,10 +1,12 @@
 from pprint import pprint
 from time import time
-from typing import Tuple, Iterator
+from typing import Tuple, Iterator, Dict
 
-from numpy import array
+from hyperopt import fmin, tpe, hp, Trials
+from numpy import array, float64
 from numpy.linalg import norm
 from scipy.special import comb
+from statsmodels.stats.proportion import proportion_confint
 
 from radius.solve_gp import solve_gp
 
@@ -19,6 +21,44 @@ def probability(n: int, k: int, p: Tuple[float, float, float]) -> float:
                 term = coefficient * (p[0] ** x1) * (p[1] ** x2) * (p[2] ** x3)
                 proba += term
     return proba
+
+
+def create_stop_fn(alpha: float):
+    def stop_fn(trials: Trials):
+        if not trials.trials:  # Check if there are any trials
+            return False, {}
+        last_loss = trials.best_trial['result']['loss']
+        return last_loss < 1 - alpha, {}
+
+    return stop_fn
+
+
+def hyperopt_optimization(n: int, k: int, L: float, timeout: int, max_evals: int, alpha: float) \
+        -> Tuple[float, Dict[str, float64]]:
+    def objective(p0: float, p1: float) -> float:
+        if (p0 + p1 > 1) or (p0 - p1 > L):
+            return 1.
+        else:
+            return probability(n, k, (p0, p1, 1 - p0 - p1))
+
+    space = {
+        'p0': hp.uniform('p0', 0, 1),
+        'p1': hp.uniform('p1', 0, 1)
+    }
+
+    trials = Trials()
+    best = fmin(fn=lambda x: objective(x['p0'], x['p1']),  # Changed this line
+                space=space,
+                algo=tpe.suggest,
+                timeout=timeout,
+                max_evals=max_evals,
+                trials=trials,
+                early_stop_fn=create_stop_fn(alpha))
+
+    # Extracting best values
+    best_value = trials.best_trial['result']['loss']
+    best_params = {key: trials.best_trial['misc']['vals'][key][0] for key in best}
+    return best_value, best_params
 
 
 def solve_signomial(x0: Tuple[float, float, float], w: Iterator[float], L: float, n: int, k: int, eps: float):
@@ -58,7 +98,11 @@ def main() -> None:
     x = (25, 3, 0)
     k = x[0] - x[1]
     n = sum(x)
-    L = 10
+    alpha = 0.001
+    p0 = proportion_confint(x[0], n, alpha=alpha, method="beta")[0]
+    p1 = proportion_confint(x[1], n, alpha=alpha, method="beta")[1]
+    L = p0 - p1
+    print(f"L = {L}")
     eps = 1e-3
     # x0 = (0.5, 0.4, 0.1)
     x0 = (0.7, 0.2, 0.1)
@@ -69,8 +113,36 @@ def main() -> None:
     print(f"Elapsed time: {time() - start_time:.2f} seconds")
     p1, p2, p3 = data['variables']['p1'], data['variables']['p2'], data['variables']['p3']
     print(f"Sum of p: {p1 + p2 + p3:.4f}")
+    print(f"Initial probability: {probability(n, k, x0):.4f}")
     print(f"Minimal probability: {probability(n, k, (p1, p2, p3)):.4f}")
 
 
+def main_hyperopt():
+    x = (25, 3, 0)
+    k = x[0] - x[1]
+    n = sum(x)
+    alpha = 0.0001
+    p0 = proportion_confint(x[0], n, alpha=alpha, method="beta")[0]
+    p1 = proportion_confint(x[1], n, alpha=alpha, method="beta")[1]
+    L = p0 - p1  # k / n
+    print(f"L = {L}")
+    # Hyperopt
+    start_time = time()
+    minimum, best_params = hyperopt_optimization(n, k, L, timeout=10, max_evals=100)
+    print(f"Elapsed time: {time() - start_time:.2f} seconds")
+    print(f"Best parameters: {best_params}")
+    print(f"Minimal probability: {minimum:.4f}")
+    p = float(best_params['p0']), float(best_params['p1']), 1 - float(best_params['p0']) - float(best_params['p1'])
+    print(
+        f"Bayesian probability: {probability(n, k, p):.4f}"
+    )
+    assert p0 - p1 <= L
+
+
 if __name__ == '__main__':
-    main()
+    while True:
+        try:
+            # main()
+            main_hyperopt()
+        except KeyboardInterrupt:
+            break
